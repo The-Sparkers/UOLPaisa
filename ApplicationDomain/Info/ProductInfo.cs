75e11c14d38ad4b5eb5c0d589a742a1688687d83
Muhammad Umair Tahir <nobelhighschool@hotmail.com>
2020-08-26 23:43:14 +0500

    set up layout

---
+++ ApplicationDomain/Info/ProductInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ApplicationDomain.Info
+{
+    public static class ProductInfo
+    {
+        static ProductInfo()
+        {
+            Name = "UL-Paisa";
+            Version = "1.0";
+            Copyright = DateTime.Now;
+        }
+        public static string Name { get; set; }
+        public static string Version { get; set; }
+        public static DateTime Copyright { get; set; }
+    }
+}
